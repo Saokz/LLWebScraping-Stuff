95a49ee94bb222e130353c9e3c660db6c2b109df
William FitzGerald <threebearsatonce@gmail.com>
2019-09-08 01:43:32 -0500

    basic functionality for scraping tagaloglessons.com

--- tagalog/sentence mining/__init__.py
+++ tagalog/sentence mining/__init__.py
@@ -1,1 +1,13 @@
-print("testing commit from different computer")
+import requests
+from bs4 import BeautifulSoup
+
+word = input("enter word to look up\n")
+url = "https://tagaloglessons.com/ajax/reference_guide_search_results2.php?keyword=" + word + "&num_results=10&no_search_login=false"
+
+data = requests.get(url)
+soup = BeautifulSoup(data.text, 'html.parser')
+div = soup.find_all('div')[3].get_text()
+extra = soup.find_all('div')[3].find_all('a')[2].get_text()
+
+result = div.split(extra)[0]
+print(result)
