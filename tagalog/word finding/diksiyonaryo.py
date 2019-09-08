e4721ccd2b46753eba53522de1420f6c9eff04e0
William <threebearsatonce@gmail.com>
2019-09-08 15:01:47 -0500

    progress on diksiyonaryo. HTML structure received not matching HTML structure in the Chrome Console

--- tagalog/word finding/diksiyonaryo.py
+++ tagalog/word finding/diksiyonaryo.py
@@ -1,16 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-word = input("enter word to look up\n")
-url = "https://diksiyonaryo.ph/search/"+word
+term = input("enter word to look up\n")
+url = "https://diksiyonaryo.ph/search/"+term
 
 data = requests.get(url)
 soup = BeautifulSoup(data.text, 'html.parser')
 #div = soup.find('div', {'class': 'sense'}).get_text()
 
-wordDiv = soup.find('div', {'class': 'word'}, {'id': word}) #right. figure out how that's supposed to work.
+word = soup.find('div', {'class': 'word'}, {'id': term})
+for sense in word.find('div', {'class': 'sense'}):
 
-for div in wordDiv:
-    contentDiv = div.find('div', {'class': 'sense-content'})
-    defDiv = contentDiv.find('span', {'class': 'definition-text'}).get_text()
-    print(defDiv)
+    sense_content = sense.find('div', {'class': 'sense-content'})
+    print(len(sense))
+        #the first "sense" div only has one element?
+        #yet the second "sense" has four?
+
+    #yet every sense is supposed to have:
+        #a sense content
+        #a definition
+        #a definition text
+    #so then what the hell?
+
