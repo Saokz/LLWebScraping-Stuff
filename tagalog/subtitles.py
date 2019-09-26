a1fa9d7efc7336c614d838fc4bdda330779b29b9
Saokz <threebearsatonce@gmail.com>
2019-09-26 15:44:49 -0500

    Add files via upload

---
+++ tagalog/subtitles.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import sys
+
+if len(sys.argv) < 3:
+    print("Error. Args required: src_file.xml, dest_file.txt")
+    sys.exit()
+
+src = sys.argv[1]
+dest = sys.argv[2]
+
+if src[-4:] != ".xml":
+    print("invalid file format")
+    sys.exit()
+if dest[-4:] != ".txt":
+    print("Unsupported destination format")
+    sys.exit()
+f = open(src, "r")
+f2 = open(dest, "w")
+soup = BeautifulSoup(f, 'xml')
+juicy_bits = soup.body
+f2.write(juicy_bits.get_text())
+
